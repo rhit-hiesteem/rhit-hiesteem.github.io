efcb250ed266c39b11f90b109dd86a69772ed506
rhit-hiesteem-123 <elliothiester@gmail.com>
2025-09-26 21:11:35 -0400

    fully finished main page

---
+++ source/main.js
@@ -0,0 +1,10 @@
+// https://stackoverflow.com/questions/31954089/how-can-i-reuse-a-navigation-bar-on-multiple-pages
+$(function(){
+  $("#nav-container").load("placeholder-html/nav.html");
+  $("#footer-container").load("placeholder-html/footer.html");
+});
+
+
+
+
+
